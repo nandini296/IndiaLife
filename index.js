9a11173173a64d5a9273d4036a5d4a300b6801c5
Nandini Bisht <nandinibisht296@gmail.com>
2023-06-23 17:27:42 +0530

    footer

--- index.js
+++ index.js
@@ -17,13 +17,24 @@ function navToggle() {
   }
 }
 
-const writeComment = document.querySelector(".write-comment");
-const subscribe = document.querySelector(".contentBx1");
-subscribe.addEventListener("click", function () {
-  subscribe.classList.toggle("active1");
+// const writeComment = document.querySelector(".write-comment");
+// const subscribe = document.querySelector(".contentBx1");
+// subscribe.addEventListener("click", function () {
+//   subscribe.classList.toggle("active1");
+// });
+
+// writeComment.addEventListener("click", function () {
+//   document.querySelector(".content2").classList.add("active2");
+// });
+
+const deleteComment = document.querySelector(".times-div");
+const shareButton = document.querySelector(".share-btn");
+shareButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  document.querySelector(".share-section").classList.toggle("active1");
 });
 
-writeComment.addEventListener("click",function(){
-  document.querySelector(".content2").classList.add("active2")
-})
-
+deleteComment.addEventListener("click", function () {
+  // this.preventDefault()
+  document.querySelector(".delete-container").classList.toggle("active1");
+});
